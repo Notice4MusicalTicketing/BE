e7e09321d19b7eba10d062201760b81ffbee593a
b2etr <102886338+betterb2etr@users.noreply.github.com>
2024-08-10 22:56:08 +0900

    샘플데이터 집어넣음
    샘플데이터 집어넣음

--- seed.ts
+++ seed.ts
@@ -6,7 +6,7 @@ async function main() {
     // 회원 정보 삽입
     await prisma.memberInfo.create({
         data: {
-            memberId: '01001',
+            memberId: '01001',  
             email: 'sara20041207@gmail.com',
             password: '12345678',
             nickname: 'Sara',
@@ -20,27 +20,27 @@ async function main() {
             musicalId: '03001',
             name: '어쩌면 해피엔딩',
             status: '공연중',
-            roundInfo: 5,
+            roundInfo: 5,  // 필드가 정의되어 있어야 합니다.
             startDate: new Date('2024-06-18'),
             endDate: new Date('2024-09-08'),
         },
     });
 
     // 찜 목록 정보 삽입
-    await prisma.Wishlist.create({
+    await prisma.wishlist.create({
         data: {
-            wishlistId: '02001',
-            memberId: '01001',
-            musicalId: '03001',
+            wishlistId: '02001',  // 숫자로 변경
+            memberId: '01001',  // 숫자로 변경
+            musicalId: '03001',  // 숫자로 변경
         },
     });
 
     // 리뷰 정보 삽입
-    await prisma.Review.create({
+    await prisma.review.create({
         data: {
-            reviewId: '04001',
-            memberId: '01001',
-            musicalId: '03001',
+            reviewId: '04001',  // 숫자로 변경
+            memberId: '01001',  // 숫자로 변경
+            musicalId: '03001',  // 숫자로 변경
             rating: 5.0,
             title: '너무 좋았어요!',
             content: '스토리와 캐스팅 모두가 너무나도 좋았어요',
